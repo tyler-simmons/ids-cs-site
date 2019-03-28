2770d52e3d729a5b758a67601a0975fce108336c
Tyler Simmons <tylersimmons@Tylers-MBP.fios-router.home>
2019-03-28 16:37:24 -0400

    ready for heroku deploy

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const {Pool, Client} = require('pg');
 const app = express();
 const port = process.env.PORT || 3000;
 
-
-app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+)
+app.use(express.static(__dirname + '/public'));
+
+const pool = new Pool({
+    user: 'qpahbqel',
+    host: 'isilo.db.elephantsql.com',
+    database: 'qpahbqel',
+    password: 'FggtLzROMfqQLA8lUZkSpM-gnkDkwukb',
+    port: 5432
+});
+
+pool.query('SELECT * FROM users', (err, res) => {
+    console.log(err, res)
+    pool.end();
+})
+
+
 
 app.get('/', (req, res) => res.render('index.ejs'));
 
